b4f4c5d1c05c236c03d4274f1fd8701e60ffdc0a
HimaBindu Bagadi <hbinduadi@gmail.com>
2023-02-28 16:29:29 -0500

    Donne fo rtuesday

---
+++ EmployeesApiSolution/EmployeesApi/SrePolicies.cs
@@ -0,0 +1,22 @@
+﻿using Polly;
+using Polly.Extensions.Http;
+
+namespace EmployeesApi;
+
+public static class SrePolicies
+{
+    public static IAsyncPolicy<HttpResponseMessage> GetDefaultRetryPolicy()
+    {
+        return HttpPolicyExtensions
+            .HandleTransientHttpError()
+            .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+            .WaitAndRetryAsync(5, retryattempt => TimeSpan.FromSeconds(Math.Pow(2, retryattempt)));
+    }
+
+    public static IAsyncPolicy<HttpResponseMessage> GetDefaultCircuitBreaker()
+    {
+        return HttpPolicyExtensions.HandleTransientHttpError()
+            .CircuitBreakerAsync(2, TimeSpan.FromSeconds(30));
+    }
+
+}
